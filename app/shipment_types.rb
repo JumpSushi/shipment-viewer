9dd5ce46faea42d7fc629b035fb3dde51d97b48e
24c02 <163450896+24c02@users.noreply.github.com>
2025-01-27 18:31:35 -0500

    add oneoff shipment type

--- app/shipment_types.rb
+++ app/shipment_types.rb
@@ -456,4 +456,29 @@ class SprigShipment < Shipment
   end
 end
 
-SHIPMENT_TYPES = [WarehouseShipment, HighSeasShipment, BobaDropsShipment, SprigShipment].freeze
+class OneOffShipment < Shipment
+  self.table_name = ENV["OO_TABLE"]
+  self.email_column = 'email'
+
+  SUPPORTED_FIELDS = %i[date tracking_number status_text icon hide_contents? status_icon shipped? description type_text title_text]
+
+  def tracking_link
+    fields['tracking_link'] || (tracking_number && "https://parcelsapp.com/en/tracking/#{tracking_number}") || nil
+  end
+
+  def date
+    fields['date'] || '2027-01-31'
+  end
+
+  def icon
+    fields['icon'] || super
+  end
+
+  SUPPORTED_FIELDS.each do |field|
+    define_method field do
+      fields[field.to_s]
+    end
+  end
+end
+
+SHIPMENT_TYPES = [WarehouseShipment, HighSeasShipment, BobaDropsShipment, SprigShipment, OneOffShipment].freeze
